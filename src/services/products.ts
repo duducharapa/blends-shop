e81715c0dcf31e54928a83360a8bd0a43aecda28
charapa <eduardocharapa@gmail.com>
2023-05-29 04:07:00 -0300

    build: add env variables for multiple environments

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,11 +1,12 @@
-import axios, { isAxiosError } from "axios";
+import { isAxiosError } from "axios";
 import Product from "../interfaces/Product";
+import api from "./api";
 
 type ListProduts = () => Promise<Product[]>;
 
 const listProducts: ListProduts = async () => {
   try {
-    const { data, status } = await axios.get<Product[]>("http://localhost:8080/products");
+    const { data, status } = await api.get<Product[]>("/products");
     if (status === 200) return data;
   } catch (err) {
     if (isAxiosError(err)) {
